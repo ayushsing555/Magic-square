c521d22fc395b41f748ed99e30dc5c98212bfed8
ayush singhal <ayushsinghalmains@gmail.com>
2022-04-06 19:29:22 +0530

    some functionality added

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+const completematrix=()=>
+{
+    var total=0;
+    for(indexofrow=0;indexofrow<3;indexofrow++){
+      var colomntotal=0;
+      for(indexofcolomn=0;indexofcolomn<3;indexofcolomn++)
+        {
+          let box_value=document.getElementById(`box${indexofrow}${indexofcolomn}`).value;
+          colomntotal += (parseInt(box_value));
+             if(colomntotal==15)
+               total++ 
+        }
+      }
+      if(total==3)
+        alert("👏 you won 🎁");
+}
 const checkmatrix=(a,b)=>{
     let row=0,coloum=0;
     for(i=0;i<3;i++)
       {
           var p=document.getElementById(`box${a}${i}`).value;
           if(p!=""){
-              
                row+=(parseInt(p));
-              DuplicateMatrix(p,a,i);
+               DuplicateMatrix(parseInt(p),a,i);
           }
           var q=document.getElementById(`box${i}${b}`).value;
           if(q!=""){
-              DuplicateMatrix(q,i,b);
+              DuplicateMatrix(parseInt(q),i,b);
               coloum+=(parseInt(q));
           }     
       }
@@ -22,12 +37,14 @@ const checkmatrix=(a,b)=>{
 }
 const input_box=document.querySelector("table");
 input_box.addEventListener("keyup",(e)=>{
+    completematrix();
     if((e.key>=0&&e.key<=9)|| e.key=="Backspace"||e.key=="Tab")
       valueUpdate(e.target.id);
     else
       alert("please press key between 0-9 😊")
 });
 function valueUpdate(targetValue){
+
     const box_id=(targetValue)
     const input_value=document.getElementById(`${box_id}`).value;
     if(input_value>15)
@@ -39,17 +56,38 @@ function analysisMatrix(a,b,row,colomn){
     if(row==15)
       for(var i=0;i<=3;i++)
         document.getElementById(`box${a}${i}`).style.color="green";
-    if(colomn==15)
+    else
+      {
+        for(var i=0;i<=3;i++)
+      document.getElementById(`box${a}${i}`).style.color="red";
+      }
+    if(colomn==15){
       for(var i=0;i<=3;i++)
         document.getElementById(`box${i}${b}`).style.color="green";
+    }
+    else{
+      for(var i=0;i<=3;i++)
+      document.getElementById(`box${i}${b}`).style.color="red";
+    } 
 }
-function DuplicateMatrix(value,iIndex,jIndex){
-    for(i=0;i<3;i++) 
-      for(j=0;j<3;j++)
-       if(i!=iIndex&&j!=jIndex){
-           if(document.getElementById(`box${i}${j}`).value==value){
+function DuplicateMatrix(inputvalue,iIndex,jIndex){
+    for(k=0;k<3;k++) 
+    {
+      for(l=0;l<3;l++){
+         if(k!==iIndex&&l!==jIndex){
+            const checkvalue=document.getElementById(`box${k}${l}`).value;
+           if(parseInt(checkvalue)==inputvalue){
+             console.log("iinedx=" + iIndex +",jindex=" + jIndex + ",k=" + k + ",j=" + l)
+             console.log("duplivcate");
              alert(" 👏Duplicate values not allowed 👏");
-             document.getElementById(`box${iIndex}${jIndex}`).value="";
+             document.getElementById(`box${k}${l}`).value="";
         }   
-       }  
+       }
+      }
+    } 
 }
+const btn=document.getElementsByTagName("button")[0];
+btn.addEventListener("click",()=>{
+    document.getElementsByClassName("non-solution")[0].style.display="none";
+    document.getElementsByClassName("solution")[0].style.display="block";
+})
